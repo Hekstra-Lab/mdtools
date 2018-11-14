Fix bug in RMSD calculation related to align_ref_atom_indices default

mdtools/analysis/observables.py
@@ -28,7 +28,7 @@ def rmsd(traj, ref, atom_indices, ref_atom_indices=None,
         provided, this defaults to atom_indices. 
     align_ref_atom_indices : np.ndarray
         Array of atomic indices in ref to use for alignment. If not
-        provided, this defaults to align_atom_indices. Must be the same 
+        provided, this defaults to ref_atom_indices. Must be the same 
         length as align_atom_indices
     frameref : int
         Frame in ref to compute RMSDs with respect to. Defaults to index
@@ -49,7 +49,7 @@ def rmsd(traj, ref, atom_indices, ref_atom_indices=None,
             align_ref_atom_indices = align_atom_indices
         else:
             align_atom_indices = atom_indices
-            align_ref_atom_indices = atom_indices
+            align_ref_atom_indices = ref_atom_indices
 
     # 1) Align to reference
     traj.superpose(ref, frameref, atom_indices=align_atom_indices,