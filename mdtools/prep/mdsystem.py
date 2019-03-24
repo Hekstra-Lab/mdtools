d6b999df59e5967328ef6306d1f9c5aa64ecd362
Jack Greisman <Jack.Greisman@gmail.com>
2019-03-24 15:51:52 -0400

    Add posre argument to MDSystem.buildSimulation()

--- mdtools/prep/mdsystem.py
+++ mdtools/prep/mdsystem.py
@@ -80,14 +80,15 @@ class MDSystem(Modeller):
         integrator = LangevinIntegrator(temperature, 1/picosecond, dt)
 
         # Add position restraints that can be tapered off during simulation
-        force = CustomExternalForce("k*((x-x0)^2+(y-y0)^2+(z-z0)^2)")
-        force.addGlobalParameter("k", 5.0*kilocalories_per_mole/angstroms**2)
-        force.addPerParticleParameter("x0")
-        force.addPerParticleParameter("y0")
-        force.addPerParticleParameter("z0")
-        for i in self.select("not water and not (element Na or element Cl) and not element H"):
-            force.addParticle(int(i), positions[i].value_in_unit(nanometers))
-        system.addForce(force)
+        if posre:
+            force = CustomExternalForce("k*((x-x0)^2+(y-y0)^2+(z-z0)^2)")
+            force.addGlobalParameter("k", 5.0*kilocalories_per_mole/angstroms**2)
+            force.addPerParticleParameter("x0")
+            force.addPerParticleParameter("y0")
+            force.addPerParticleParameter("z0")
+            for i in self.select("not water and not (element Na or element Cl) and not element H"):
+                force.addParticle(int(i), positions[i].value_in_unit(nanometers))
+            system.addForce(force)
         
         # Setup barostat for NPT ensemble
         if ensemble == "NPT":
