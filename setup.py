5176dc962ff12d1fb2aba429f57d51e98238ea9a
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-02 18:11:23 -0400

    Fix setup.py to include all packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mdtools',
     version='0.1.0',
     author='Jack B. Greisman',
     author_email='greisman@g.harvard.edu',
-    packages=['mdtools'],
+    packages=find_packages(),
     description='Tools for running MD simulation in OpenMM',
     install_requires=[
         "numpy >= 1.6",
