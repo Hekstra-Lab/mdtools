fde7fdb83c85cfa2051420010cd6302e81bb2528
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-15 12:52:17 -0400

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mdtools',
-    version='0.1.0',
+    version='0.1.1',
     author='Jack B. Greisman',
     author_email='greisman@g.harvard.edu',
     packages=find_packages(),
