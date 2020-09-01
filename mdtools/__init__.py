75bd7f1ee04264d5e611337aff6a935c4e541637
Jack Greisman <Jack.Greisman@gmail.com>
2020-09-01 17:33:22 -0400

    Restructure how version numbers are specified in mdtools

--- mdtools/__init__.py
+++ mdtools/__init__.py
@@ -1,3 +1,10 @@
+# Version number for mdtools
+def getVersionNumber():
+    import pkg_resources
+    version = pkg_resources.require("mdtools")[0].version
+    return version
+__version__ = getVersionNumber()
+
 from .prep.mdsystem import MDSystem
 from .prep.solvatedmdsystem import SolvatedMDSystem
 from .prep.latticemdsystem import  LatticeMDSystem
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+# Get version number
+def getVersionNumber():
+    with open("mdtools/VERSION", 'r') as vfile:
+        version = vfile.read().strip()
+    return version
+__version__ = getVersionNumber()
+
 setup(
     name='mdtools',
-    version='0.1.1',
+    version=__version__,
     author='Jack B. Greisman',
     author_email='greisman@g.harvard.edu',
     packages=find_packages(),
