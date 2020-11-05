19eb6c0aed6363ff0591cef49e299f342d286362
Jack Greisman <Jack.Greisman@gmail.com>
2020-11-05 16:45:02 -0500

    Add default thermalize=True to MDSystem.buildSimulation()

--- mdtools/prep/mdsystem.py
+++ mdtools/prep/mdsystem.py
@@ -86,7 +86,7 @@ class MDSystem(Modeller):
                         efx=False, ef=(0,0,0), ef_sel="all", nonbondedMethod=PME,
                         nonbondedCutoff=1.*nanometer, constraints=HBonds, rigidWater=True, exceptions=[],
                         filePrefix="traj", saveTrajectory=False, trajInterval=500,
-                        saveStateData=False, stateDataInterval=250, atomSubset=None):
+                        saveStateData=False, stateDataInterval=250, atomSubset=None, thermalize=True):
         """
         Build a simulation context from the system. The simulation is
         then available as an attribute.
@@ -145,10 +145,12 @@ class MDSystem(Modeller):
 
         # Add simulation
         self.simulation = Simulation(self.topology, system, integrator)
-
-        # Initialize particle positions
+        
+        # Initialize particle positions and velocities
         self.simulation.context.setPositions(self.positions)
-
+        if thermalize:
+            self.thermalize(temperature=temperature)
+        
         # Add reporters
         if saveTrajectory:
             self.simulation.reporters.append(HDF5Reporter(f"{filePrefix}.h5", trajInterval, atomSubset=atomSubset))
